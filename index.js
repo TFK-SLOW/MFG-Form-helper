601195a205c414c3aab2dc7d52c96cc2aa917d30
TFK-SLOW <kz.guira08@gmail.com>
2025-06-18 10:52:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ app.get("/webhook", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Handle incoming messages
